97b4d7317bcb4aa121c3421d76903303c171d41a
Alan S <szuszkiewiczalan928@gmail.com>
2020-11-26 10:08:28 +0100

    Updated method of adding data to csv file

--- Statistical Program/gui.py
+++ Statistical Program/gui.py
@@ -1,17 +1,30 @@
 from tkinter import *
+import csv
+import pandas as pd
 
 root = Tk()
 L1 = Label(root, text="Enter Data Separated by commas")
 L1.pack(side=LEFT)
 E1 = Entry(root, bd=5)
 E1.pack(side=RIGHT)
+tlist = []
 
 
-def test():
-    print(E1.get())
+def toarr():
+    string = E1.get()
+    print("Values Entered: " + string)
+    global tlist
+    tlist = string.split(",")
+    print(tlist)
+    addtocsv()
 
 
-button_disp = Button(root, text="Show", command=test)
+def addtocsv():
+    df = pd.DataFrame(tlist, columns=["colummn"])
+    df.to_csv('list.csv', index=False)
+
+
+button_disp = Button(root, text="Show", command=toarr)
 button_disp.pack()
 
 root.mainloop()
